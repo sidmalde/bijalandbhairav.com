ae1be2839646b18ef4e2085368d59af07dd0bb9d
Sid Malde <sidmalde@gmail.com>
2016-07-04 18:16:08 +0100

    update

--- app/Config/database.php
+++ app/Config/database.php
@@ -17,16 +17,16 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'sidmalde',
-		'password' => 'F3rrari1',
+		'password' => 'El3phant',
 		'database' => 'bijalandbhairav',
 		'prefix' => '',
 	);
 	
 	function __construct(){
-		if (strpos($_SERVER['HTTP_HOST'],'www.') !== false) {
-			$this->default = $this->live;
-		} else {
+		if (strpos($_SERVER['HTTP_HOST'],'dev.') !== false) {
 			$this->default = $this->default;
+		} else {
+			$this->default = $this->live;
 		}
 		// $this->default = $this->default;
 	}
